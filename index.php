41915bf454281d8392f05865fdff595ee38459fa
RomainLefranc <lefranc46@gmail.com>
2020-11-11 22:42:13 +0400

    v1.1

--- index.php
+++ index.php
@@ -5,20 +5,20 @@ session_start();
 require_once __DIR__ . '/Facebook/autoload.php';
 
 $navigations = array (
-    [          'action' => "c",       "NiveauAcces" => 0,     "controller" => "c_connexion",                        "view" => "v_connexion"            ],
-    [          'action' => "a",       "NiveauAcces" => 1,     "controller" => "c_admin",                            "view" => "v_admin"                ],
-    [          'action' => "l",       "NiveauAcces" => 1,     "controller" => "c_lier",                             "view" => "v_lier"                 ],
-    [          'action' => "d",       "NiveauAcces" => 1,     "controller" => "c_deconnexion",                      "view" => ""                       ],
+    ['action' => "c",       "NiveauAcces" => 0,     "controller" => "c_connexion",                        "view" => "v_connexion"            ],
+    ['action' => "a",       "NiveauAcces" => 1,     "controller" => "c_admin",                            "view" => "v_admin"                ],
+    ['action' => "l",       "NiveauAcces" => 1,     "controller" => "c_lier",                             "view" => "v_lier"                 ],
+    ['action' => "d",       "NiveauAcces" => 1,     "controller" => "c_deconnexion",                      "view" => ""                       ],
     /* Instagram */
-    [          'action' => "ib",      "NiveauAcces" => 1,     "controller" => "instagram/c_instagram_bilan",        "view" => "instagram/v_bilan"      ],
-    [          'action' => "icsv",    "NiveauAcces" => 1,     "controller" => "instagram/c_instagram_csv",          "view" => "instagram/v_csv"        ],
-    [          'action' => "ie",      "NiveauAcces" => 1,     "controller" => "instagram/c_instagram_exporter",     "view" => "instagram/v_exporter"   ],
-    [          'action' => "if",      "NiveauAcces" => 1,     "controller" => "instagram/c_instagram_comparatif",   "view" => "instagram/v_comparatif" ],
+    ['action' => "ib",      "NiveauAcces" => 1,     "controller" => "instagram/c_instagram_bilan",        "view" => "instagram/v_bilan"      ],
+    ['action' => "icsv",    "NiveauAcces" => 1,     "controller" => "instagram/c_instagram_csv",          "view" => "instagram/v_csv"        ],
+    ['action' => "ie",      "NiveauAcces" => 1,     "controller" => "instagram/c_instagram_exporter",     "view" => "instagram/v_exporter"   ],
+    ['action' => "if",      "NiveauAcces" => 1,     "controller" => "instagram/c_instagram_comparatif",   "view" => "instagram/v_comparatif" ],
     /* Facebook */
-    [          'action' => "fb",      "NiveauAcces" => 1,     "controller" => "facebook/c_facebook_bilan",          "view" => "facebook/v_bilan"       ],
-    [          'action' => "fcsv",    "NiveauAcces" => 1,     "controller" => "facebook/c_facebook_csv" ,           "view" => "facebook/v_csv"         ],
-    [          'action' => "fe",      "NiveauAcces" => 1,     "controller" => "facebook/c_facebook_exporter" ,      "view" => "facebook/v_exporter"    ],
-    [          'action' => "ff",      "NiveauAcces" => 1,     "controller" => "facebook/c_facebook_comparatif" ,    "view" => "facebook/v_comparatif"  ]
+    ['action' => "fb",      "NiveauAcces" => 1,     "controller" => "facebook/c_facebook_bilan",          "view" => "facebook/v_bilan"       ],
+    ['action' => "fcsv",    "NiveauAcces" => 1,     "controller" => "facebook/c_facebook_csv" ,           "view" => "facebook/v_csv"         ],
+    ['action' => "fe",      "NiveauAcces" => 1,     "controller" => "facebook/c_facebook_exporter" ,      "view" => "facebook/v_exporter"    ],
+    ['action' => "ff",      "NiveauAcces" => 1,     "controller" => "facebook/c_facebook_comparatif" ,    "view" => "facebook/v_comparatif"  ]
 );
 
 $actionEstValide = false;
@@ -41,7 +41,6 @@ if (isset($_GET["a"])) {
             $view = $navigation['view'];
             include 'controller/controller'.$acces.'.php';
             include 'view/view'.$acces.'.php';
-            
         }
     }
     if (!$actionEstValide) {
