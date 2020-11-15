8953661b9f87a6a74ff953a9281fe2c1c2b446f5
RomainLefranc <lefranc46@gmail.com>
2020-11-15 21:07:58 +0400

    v1.1

--- view/admin/instagram/v_bilan.php
+++ view/admin/instagram/v_bilan.php
@@ -50,20 +50,20 @@
                     </div>
                     <form id='bilan'>
                         <div class="form-group row">
-                            <label for="example-page-input" class="col-lg-2 col-form-label">Pages disponible</label>
+                            <label class="col-lg-2 col-form-label">Pages disponible</label>
                             <div class="col-lg-10">
-                            <select class="form-control" id='choixPageInsta' style="max-width: 300px;" required>
-                            <option value="null"  data-nom=""> </option>
-                            <?php
-                                echo $selectPageInsta;
-                            ?>
-                            </select>
+                                <select class="form-control" id='choixPageInsta' style="max-width: 300px;" required>
+                                    <option></option>
+                                    <?php
+                                        echo $selectPageInsta;
+                                    ?>
+                                </select>
                             </div>
                         </div>
                         <div class="form-group row" >
-                            <label for="example-date-input" class="col-lg-2 col-form-label">Date minimum</label>
+                            <label class="col-lg-2 col-form-label">Date minimum</label>
                             <div class="col-lg-10">
-                            <input class="form-control" type="date" value="<?php echo date('Y-m-d'); ?>" id="dateDebut" style="max-width: 300px;">
+                                <input class="form-control" type="date" id="dateDebut" style="max-width: 300px;" required>
                             </div>
                         </div> 
                         <button type="submit" class="btn btn-primary">Bilan</button>
@@ -76,122 +76,86 @@
                             $('#result').html('');
                             $('#erreur').html('');
                             var idPageInsta = $('#choixPageInsta').val();
-
                             var token = $('#choixPageInsta').find('option:selected').data('value');
-
                             var dateDebut = $('#dateDebut').val();  
                             function msgErreur(texte) {
-                            var alert = `
-                            <div class="alert alert-danger alert-dismissible fade show mt-3" role="alert" >
-                                <strong>Erreur !</strong> ${texte}
-                                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                                </button>
-                            </div>`;
-                            return alert;
-                            
+                                var alert = `
+                                <div class="alert alert-danger alert-dismissible fade show mt-3" role="alert" >
+                                    <strong>Erreur !</strong> ${texte}
+                                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>`;
+                                return alert;
                             }
-                            var url = `https://graph.facebook.com/v8.0/${idPageInsta}?fields=id,media{id,like_count,media_type,comments_count,thumbnail_url,media_url,timestamp}&access_token=${token}`;
-
-                            if ($('#choixPageInsta').find('option:selected').data('nom') == '') {
-                            
-                            $('#erreur').html(msgErreur("Veuillez selectionner une page Instagram"));
-                            } else {
-                            $.get(url, function (data, textStatus) {
-                                
+                            $.get(`https://graph.facebook.com/v8.0/${idPageInsta}?fields=id,media{id,like_count,media_type,comments_count,thumbnail_url,media_url,timestamp}&access_token=${token}`, function (data, textStatus) {
                                 switch (textStatus) {
-                                case 'success':
-                                    nbMedia = 0
-                                    if (data.media !== undefined ) {
-                                    data.media.data.forEach(media => {
-                                        var date = new Date(media.timestamp);
-                                        var dateFormatte = ((date.getDate() > 9) ? date.getDate() : ('0' + date.getDate())) + '/' + ((date.getMonth() > 8) ? (date.getMonth() + 1) : ('0' + (date.getMonth() + 1))) + '/' + date.getFullYear() + ' à ' + date.getHours() + 'h' +  ((date.getMinutes() > 9) ? date.getMinutes() : ('0' + date.getMinutes()));
-                                        if (media.timestamp > dateDebut) {
-                                        nbMedia++;
-                                        var dateMedia = dateFormatte;
-                                        var nbLike = media.like_count;
-                                        var nbComments = media.comments_count;
-                                        var idMedia = media.id;
-                                        switch (media.media_type) {
-                                            case 'VIDEO':
-                                            var img = media.thumbnail_url;
-                                            var url =`https://graph.facebook.com/v8.0/${idMedia}/insights?metric=impressions,reach,video_views&access_token=${token}`;
-                                            $.ajax({
-                                                url: url,
-                                                dataType: "json",
-                                                async: false,
-                                                success: function (response) {
-                                                var impression = response.data[0].values[0].value;
-                                                var reach = response.data[1].values[0].value;
-                                                var nbVue = response.data[2].values[0].value;
-                                                var carte = `
-                                                    <div class="card m-2" style="width: 18rem;">
-                                                        <img class="card-img-top" src="${img}">
-                                                        <div class="card-body">
-                                                            <p class="card-text">
-                                                                <strong>Date : </strong>${dateFormatte}<br>
-                                                                <strong>like : </strong>${nbLike}<br>
-                                                                <strong>Commentaire : </strong>${nbComments}<br>
-                                                                <strong>Impression : </strong>${impression}<br>
-                                                                <strong>Vue videos : </strong>${nbVue}<br>
-                                                                <strong>Reach : </strong>${reach}<br>
-                                                                <strong>Taux d'engagement </strong>${(((nbLike + nbComments)/reach)*100).toFixed(2)+' %'}
-                                                            </p>
-                                                        </div>
-                                                    </div>
-                                                    `;
-                                                $( "#result" ).append(carte);
+                                    case 'success':
+                                        nbMedia = 0
+                                        data.media.data.forEach(media => {
+                                            var date = new Date(media.timestamp);
+                                            var dateFormatte = ((date.getDate() > 9) ? date.getDate() : ('0' + date.getDate())) + '/' + ((date.getMonth() > 8) ? (date.getMonth() + 1) : ('0' + (date.getMonth() + 1))) + '/' + date.getFullYear() + ' à ' + date.getHours() + 'h' +  ((date.getMinutes() > 9) ? date.getMinutes() : ('0' + date.getMinutes()));
+                                            if (media.timestamp > dateDebut) {
+                                                nbMedia++;
+                                                var dateMedia = dateFormatte;
+                                                var nbLike = media.like_count;
+                                                var nbComments = media.comments_count;
+                                                var idMedia = media.id;
+                                                switch (media.media_type) {
+                                                    case 'VIDEO':
+                                                        var img = media.thumbnail_url;
+                                                        var url =`https://graph.facebook.com/v8.0/${idMedia}/insights?metric=impressions,reach,video_views&access_token=${token}`;
+                                                        break;
+                                                
+                                                    default:
+                                                        var img = media.media_url;
+                                                        var url =`https://graph.facebook.com/v8.0/${idMedia}/insights?metric=impressions,reach&access_token=${token}`;
+                                                        break;
                                                 }
-                                            });
-                                            break;
-                                        
-                                            default:
-                                            var img = media.media_url;
-                                            var url =`https://graph.facebook.com/v8.0/${idMedia}/insights?metric=impressions,reach&access_token=${token}`;
-                                            $.ajax({
-                                                url: url,
-                                                dataType: "json",
-                                                async: false,
-                                                success: function (response) {
-                                                var impression = response.data[0].values[0].value;
-                                                var reach = response.data[1].values[0].value;
-                                                var carte = `
-                                                    <div class="card m-2" style="width: 18rem;">
-                                                        <img class="card-img-top" src="${img}">
-                                                        <div class="card-body">
-                                                            <p class="card-text">
-                                                                <strong>Date : </strong>${dateFormatte}<br>
-                                                                <strong>like : </strong>${nbLike}<br>
-                                                                <strong>Commentaire : </strong>${nbComments}<br>
-                                                                <strong>Impression : </strong>${impression}<br>
-                                                                <strong>Reach : </strong>${reach}<br>
-                                                                <strong>Taux d'engagement </strong>${(((nbLike + nbComments)/reach)*100).toFixed(2)+' %'}<br>
-                                                            </p>
+                                                $.ajax({
+                                                    url: url,
+                                                    dataType: "json",
+                                                    async: false,
+                                                    success: function (response) {
+                                                    var impression = response.data[0].values[0].value;
+                                                    var reach = response.data[1].values[0].value;
+                                                    var ligne = ""
+                                                    var nbVue = 0;
+                                                    if (response.data.length >= 3) {
+                                                        nbVue = response.data[2].values[0].value
+                                                        ligne = `<strong>Vue videos : </strong>${nbVue}<br>`
+                                                    }
+                                                    var carte = `
+                                                        <div class="card m-2" style="width: 18rem;">
+                                                            <img class="card-img-top" src="${img}">
+                                                            <div class="card-body">
+                                                                <p class="card-text">
+                                                                    <strong>Date : </strong>${dateFormatte}<br>
+                                                                    <strong>like : </strong>${nbLike}<br>
+                                                                    <strong>Commentaire : </strong>${nbComments}<br>
+                                                                    <strong>Impression : </strong>${impression}<br>
+                                                                    ${ligne}
+                                                                    <strong>Reach : </strong>${reach}<br>
+                                                                    <strong>Taux d'engagement </strong>${(((nbLike + nbComments)/reach)*100).toFixed(2)+' %'}
+                                                                </p>
+                                                            </div>
                                                         </div>
-                                                    </div>
-                                                    `;
-                                                $( "#result" ).append(carte);
-                                                }
-                                            });
-                                            break;
+                                                        `;
+                                                    $( "#result" ).append(carte);
+                                                    }
+                                                });
+                                            }
+                                        });
+                                        if (nbMedia == 0) {
+                                            $('#erreur').html(msgErreur('Aucun post pour cette periode, veuillez choisir une date plus ancienne'));
                                         }
-                                        }
-                                    });
-                                    if (nbMedia == 0) {
-                                        $('#erreur').html(msgErreur('Aucun post pour cette periode, veuillez choisir une date plus ancienne'));
-                                        
-                                    }
-                                    } else {
-                                    $('#erreur').html(msgErreur(`Il n'y a aucun post sur cette page !`));
-                                    }
-                                    break;
-                                
-                                default:
-                                    $('#erreur').html(msgErreur("Serveur indisponible ! Veuillez réessayer plus tard ou reliez votre compte à l'application"));      
-                                    break;
+                                        break;
+                                    
+                                    default:
+                                        $('#erreur').html(msgErreur("Serveur indisponible ! Veuillez réessayer plus tard ou reliez votre compte à l'application"));      
+                                        break;
                                 }  
                             });
-                            }
                         });
                     </script>
                 </div>
