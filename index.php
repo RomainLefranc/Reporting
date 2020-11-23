93e39405be49733f5d29b9138b46013971655361
RomainLefranc <lefranc46@gmail.com>
2020-11-23 15:44:26 +0400

    v1.1

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 session_start();
 
-require_once __DIR__ . '/Facebook/autoload.php';
+require_once __DIR__ . '/vendor/Facebook/autoload.php';
 
 $navigations = array (
     ['action' => "c",    "NiveauAcces" => 0,  "controller" => "c_connexion",                      "view" => "v_connexion"            ],
