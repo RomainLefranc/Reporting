e305f400e2e66c147c20af4c1575af7be759cec6
RomainLefranc <lefranc46@gmail.com>
2020-11-23 15:00:34 +0400

    v1.1

--- controller/public/c_connexion.php
+++ controller/public/c_connexion.php
@@ -1,12 +1,13 @@
 <?php
 
     include "model/m_utilisateurs.php";
-    
-    if (isset($_POST["id"]) && isset($_POST['mdp'])) {
+    if (isset($_SESSION['user'])) {
+        header ('location: index.php?a=a'); 
+    } elseif (isset($_POST["id"]) && isset($_POST['mdp'])) {
         require_once 'vendor/google/recaptcha/src/autoload.php';
         $recaptcha = new \ReCaptcha\ReCaptcha('6LeQn-EZAAAAAJIPWyMdK0NjEGgmIGbIEWti3_Ee');
-     
-        $resp = $recaptcha->verify($_POST['g-recaptcha-response']);
+        $cleCaptcha = htmlspecialchars($_POST['g-recaptcha-response']);
+        $resp = $recaptcha->verify($cleCaptcha);
         if ($resp->isSuccess()) {
             $id = htmlspecialchars($_POST['id']);
             $mdp = htmlspecialchars($_POST['mdp']);
